a31cd16a7cdb02938eed7251eba71fc1499bb144
Rei Setogawa <rset6207@uni.sydney.edu.au>
2023-06-07 21:20:35 +1000

    Added mobile version of the web app to make it responsive

--- public/script.js
+++ public/script.js
@@ -4,10 +4,11 @@ var sessions = [];
 function addSession() {
   var session = document.getElementById("add_info");
   if (session.style.display === "none") {
-    session.style.display = "block";
+    session.style.display = "flex";
   } else {
     session.style.display = "none";
   }
+
 }
 
 // Function to add an exercise to the session
@@ -79,15 +80,19 @@ function saveSession() {
     name: sessionName,
     date: sessionDate,
     exercises: exercises,
-    performance: sessionPerf
+    performance: sessionPerf,
   };
 
+  sessionData.emoji = getEmoji(sessionPerf);
+
   sessions.push(sessionData);
+  localStorage.setItem("sessions", JSON.stringify(sessions));
 
 
   displaySessions();
   updateSessionCount();
-  updatePrePerformance()
+  updatePrePerformance();
+  displaySessionPerformance();
 
   clearFields();
 
@@ -95,6 +100,31 @@ function saveSession() {
   session.style.display = "none";
 
   console.log(sessionData); // Log all saved sessions
+}
+
+function retrieveSessions() {
+  var storedSessions = localStorage.getItem("sessions");
+  if (storedSessions) {
+    sessions = JSON.parse(storedSessions);
+    displaySessions();
+    updateSessionCount();
+    updatePrePerformance();
+    displaySessionPerformance();
+  }
+}
+
+window.addEventListener("load", retrieveSessions);
+
+function getEmoji(performance) {
+  if (performance === 'Average') {
+    return '&#128515;'; // Smiley face emoji
+  } else if (performance === 'Poor') {
+    return '&#128517;'; // Frowning face emoji
+  } else if (performance === 'Good') {
+    return '&#128513;'; // Grinning face emoji
+  } else {
+    return ''; // Default empty string if performance is not recognized
+  }
 }
 
 // Function to display the sessions
@@ -106,7 +136,10 @@ function displaySessions() {
     var session = sessions[i];
 
     var sessionDiv = document.createElement("div");
-    sessionDiv.classList.add("session-item");
+    sessionDiv.classList.add("session-item", "session-div-hover");
+
+    var sessionInfoDiv = document.createElement("div"); // Div to hold sessionNum, sessionName, sessionDate, and sessionPerf
+    sessionInfoDiv.classList.add("session-info");
 
     var sessionNum = document.createElement("p");
     sessionNum.classList.add("session-num");
@@ -115,22 +148,28 @@ function displaySessions() {
     var sessionName = document.createElement("h3");
     sessionName.innerHTML = session.name;
 
-    var sessionDelete = document.createElement("button");
-    sessionDelete.innerHTML = "Delete";
-    sessionDelete.addEventListener("click", createDeleteHandler(i));
-
     var sessionDate = document.createElement("p");
     sessionDate.innerHTML = session.date;
 
-    var sessionPerf = document.createElement("p");
-    sessionPerf.innerHTML = session.performance;
+    var sessionPerf = document.createElement("span");
+    sessionPerf.innerHTML = session.performance + "" + session.emoji;
 
-    var saveSession = document.createElement("p");
-    saveSession.innerHTML = session.performance;
+    sessionInfoDiv.appendChild(sessionNum);
+    sessionInfoDiv.appendChild(sessionName);
+    sessionInfoDiv.appendChild(sessionDate);
+    sessionInfoDiv.appendChild(sessionPerf);
 
-    var exerciseList = document.createElement("ul");
-    exerciseList.classList.add("hidden");
+    var sessionDelete = document.createElement("div"); // Div to hold sessionDelete button
+    sessionDelete.classList.add("session-delete");
 
+    var deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.addEventListener("click", createDeleteHandler(i));
+
+    sessionDelete.appendChild(deleteButton);
+
+    var exerciseList = document.createElement("div"); // Div to hold exerciseList
+    exerciseList.classList.add("exercise-list");
 
     for (var j = 0; j < session.exercises.length; j++) {
       var exerciseData = session.exercises[j];
@@ -139,23 +178,19 @@ function displaySessions() {
       var exerciseName = document.createElement("p");
       exerciseName.innerHTML = exerciseData.name;
       var exerciseWeight = document.createElement("p");
-      exerciseWeight.innerHTML = exerciseData.weight;
+      exerciseWeight.innerHTML = exerciseData.weight + "kgs";
       var exerciseNum = document.createElement("p");
-      exerciseNum.innerHTML = exerciseData.num;
+      exerciseNum.innerHTML = "Sets/Reps:" + " " + exerciseData.num;
 
-    
       exerciseItem.appendChild(exerciseName);
       exerciseItem.appendChild(exerciseWeight);
       exerciseItem.appendChild(exerciseNum);
       exerciseList.appendChild(exerciseItem);
     }
 
-    sessionDiv.appendChild(sessionNum);
-    sessionDiv.appendChild(sessionName);
-    sessionDiv.appendChild(sessionDate);
-    sessionDiv.appendChild(sessionPerf);
-    sessionDiv.appendChild(exerciseList);
+    sessionDiv.appendChild(sessionInfoDiv);
     sessionDiv.appendChild(sessionDelete);
+    sessionDiv.appendChild(exerciseList);
 
     sessionDiv.addEventListener("click", createExpandHandler(sessionDiv));
 
@@ -169,84 +204,121 @@ function createDeleteHandler(index) {
     sessions.splice(index, 1);
     displaySessions();
     updateSessionCount();
-    updatePrePerformance()
+    updatePrePerformance();
+    displaySessionPerformance();
   };
 }
 
 // Function to create an expand handler for a session
 function createExpandHandler(sessionDiv) {
   return function () {
-    var exerciseList = sessionDiv.querySelector("ul");
-    exerciseList.classList.toggle("hidden");
+    var exerciseList = sessionDiv.querySelector("div.exercise-list");
+    if (exerciseList.style.display ==="none") {
+      exerciseList.style.display="flex";
+    } else {
+      exerciseList.style.display="none";
+    }
   };
 }
 
-//To display Num of sessions 
+// To display Num of sessions 
 function updateSessionCount() {
-    var sessionsList = document.getElementById("sessions_list");
-    var numSessions = sessionsList.children.length;
+  var sessionsList = document.getElementById("sessions_list");
+  var numSessions = sessionsList.children.length;
 
-    var numSessionsElement = document.getElementById("num_of_sess");
-    numSessionsElement.innerHTML = numSessions;
+  var numSessionsElement = document.getElementById("num_of_sess");
+  numSessionsElement.innerHTML = numSessions;
 }
 
+// To update the previous performance
 function updatePrePerformance() {
-    // Get the index of the most recent session
-    var latestSessionIndex = sessions.length - 1;
-  
-    // Check if there is any session data
-    if (latestSessionIndex >= 0) {
-      // Get the most recent session performance
-      var latestSessionPerf = sessions[latestSessionIndex].performance;
-  
-      // Update the pre_perf element with the latest session performance
-      document.getElementById("pre_perf").innerHTML = latestSessionPerf;
-    }
-  }
-  
+  var latestSessionIndex = sessions.length - 1;
+  if (latestSessionIndex >= 0) {
+    var latestSessionPerf = sessions[latestSessionIndex].performance;
+    var latestSessionEmoji = sessions[latestSessionIndex].emoji;
 
-//FOR TIME 
+    var prePerfElement = document.getElementById("pre_perf");
+    prePerfElement.innerHTML = ""; // Clear the previous content
+
+    var perfSpan = document.createElement("span");
+    perfSpan.innerHTML = latestSessionPerf;
+
+    var emojiSpan = document.createElement("span");
+    emojiSpan.innerHTML = latestSessionEmoji;
+
+    prePerfElement.appendChild(perfSpan);
+    prePerfElement.appendChild(emojiSpan);
+  }
+}
+
+// To display session performance
+function displaySessionPerformance() {
+  var performanceContainer = document.querySelector(".performance_container");
+  performanceContainer.innerHTML = ""; // Clear the container before populating it again
+
+  for (var i = 0; i < sessions.length; i++) {
+    var session = sessions[i];
+
+    var sessionDiv = document.createElement("div");
+    sessionDiv.classList.add("session-performance");
+
+    var sessionDate = document.createElement("p");
+    sessionDate.innerHTML = session.date;
+
+    var sessionPerf = document.createElement("p");
+    sessionPerf.innerHTML = session.performance;
+
+    var sessionEmoji = document.createElement("span");
+    sessionEmoji.innerHTML = session.emoji;
+
+    sessionDiv.appendChild(sessionDate);
+    sessionDiv.appendChild(sessionPerf);
+    sessionDiv.appendChild(sessionEmoji);
+    performanceContainer.appendChild(sessionDiv);
+  }
+}
 function updateDate() {
-    let today = new Date();
-  
-    // return number
-    let dayName = today.getDay(),
-      dayNum = today.getDate(),
-      month = today.getMonth(),
-      year = today.getFullYear();
-  
-    const months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    const dayWeek = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    // value -> ID of the html element
-    const IDCollection = ["day", "daynum", "month", "year"];
-    // return value array with number as a index
-    const val = [dayWeek[dayName], dayNum, months[month], year];
-    for (let i = 0; i < IDCollection.length; i++) {
-      document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i];
-    }
-  }
-  
-  updateDate();
+  let today = new Date();
 
-  
+  // return number
+  let dayName = today.getDay(),
+    dayNum = today.getDate(),
+    month = today.getMonth(),
+    year = today.getFullYear();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const dayWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  // value -> ID of the html element
+  const IDCollection = ["day", "daynum", "month", "year"];
+  // return value array with number as a index
+  const val = [dayWeek[dayName], dayNum, months[month], year];
+  for (let i = 0; i < IDCollection.length; i++) {
+    document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i];
+  }
+}
+// Update the date when the page loads
+updateDate();
+
+// Add an event listener to the "Add Session" button
+
